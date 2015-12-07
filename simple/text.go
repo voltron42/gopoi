a1ed030975c786e1e43d77a516c1ba1542529133
Daniel Johnson <daniel.johnson@surescripts.com>
2015-12-07 12:48:44 -0600

    adding text objects

--- simple/text.go
+++ simple/text.go
@@ -14,17 +14,33 @@ func (t Text) Draw(ctx *context) error {
 	return ctx.pdf.Error()
 }
 
+type Cell struct {
+	TextString string     `xml:"string,attr"`
+	CellMargin *float64   `xml:"margin,attr"`
+	Point      Coordinate `xml:"point"`
+	TextStyle  *TextStyle `xml:"text-style"`
+}
+
+func (t Cell) Draw(ctx *context) error {
+	if t.TextStyle != nil {
+		t.TextStyle.set(ctx)
+	}
+	if t.CellMargin != nil {
+		ctx.pdf.SetCellMargin(*t.CellMargin)
+	}
+	ctx.pdf.Cell(t.Point.X, t.Point.Y, t.TextString)
+	return ctx.pdf.Error()
+}
+
 /*
 func (f *Fpdf) Ln(h float64)
 func (f *Fpdf) Bookmark(txtStr string, level int, y float64)
-func (f *Fpdf) Cell(w, h float64, txtStr string)
 func (f *Fpdf) CellFormat(w, h float64, txtStr string, borderStr string, ln int, alignStr string, fill bool, link int, linkStr string)
 func (f *Fpdf) Cellf(w, h float64, fmtStr string, args ...interface{})
 func (f *Fpdf) MultiCell(w, h float64, txtStr, borderStr, alignStr string, fill bool)
 func (f *Fpdf) RawWriteBuf(buf *bytes.Buffer)
 func (f *Fpdf) RawWriteStr(str string)
 func (f *Fpdf) SetCellMargin(margin float64)
-func (f *Fpdf) Text(x, y float64, txtStr string)
 func (f *Fpdf) Write(h float64, txtStr string)
 func (f *Fpdf) WriteAligned(width, lineHeight float64, textStr, alignStr string)
 func (f *Fpdf) WriteLinkID(h float64, displayStr string, linkID int)
